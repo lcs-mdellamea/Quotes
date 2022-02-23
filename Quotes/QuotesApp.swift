b7d53c7be4b41f1e7767b24eb8c2d538bb378ad9
Madison Dellamea <mdellamea@lcs.on.ca>
2022-02-22 21:16:03 -0600

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Madison Dellamea on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
